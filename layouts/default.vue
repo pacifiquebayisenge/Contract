597a94ad908dcc75d47b8671aca2a68feaa69a41
Honore Pacifique Bayisenge <hpacifique@live.be>
2025-10-09 17:32:11 +0200

    browser theme color test

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,25 @@
 <template>
-  <div class="test">
-    <NModalProvider>
-      <div id="app-container" class="flex flex-col min-h-screen">
-        <div
-          class="sticky top-0 z-50 flex justify-center items-center py-2 bg-white"
-          style="border-top-left-radius: 1.5rem; border-top-right-radius: 1.5rem"
-        >
-          <TopNavBar />
-        </div>
-        <!-- Page content -->
-        <div class="flex-1 px-6 py-2 pb-18">
-          <slot />
-        </div>
-
-        <div class="sticky bottom-10 z-50 flex justify-center items-center py-4">
-          <BottomMenuBar />
-        </div>
-
-        <!-- PWA Status Component -->
-        <PwaStatus />
+  <NModalProvider>
+    <div id="app-container" class="flex flex-col min-h-screen">
+      <div
+        class="sticky top-0 z-50 flex justify-center items-center py-2 bg-white"
+        style="border-top-left-radius: 1.5rem; border-top-right-radius: 1.5rem"
+      >
+        <TopNavBar />
       </div>
-    </NModalProvider>
-  </div>
+      <!-- Page content -->
+      <div class="flex-1 px-6 py-2 pb-18">
+        <slot />
+      </div>
+
+      <div class="sticky bottom-10 z-50 flex justify-center items-center py-4">
+        <BottomMenuBar />
+      </div>
+
+      <!-- PWA Status Component -->
+      <PwaStatus />
+    </div>
+  </NModalProvider>
 </template>
 
 <script setup>
@@ -30,9 +28,6 @@ import BottomMenuBar from "~/components/BottomMenuBar.vue";
 import TopNavBar from "~/components/TopNavBar.vue";
 </script>
 <style lang="scss">
-.test {
-  background-color: red;
-}
 div#app-container {
   border-top-left-radius: 1.5rem;
   border-top-right-radius: 1.5rem;
