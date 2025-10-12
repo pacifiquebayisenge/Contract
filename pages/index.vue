3d8b6e3a7c88c6446421402e876459a76e16c3ed
Honore Pacifique Bayisenge <hpacifique@live.be>
2025-10-12 21:57:58 +0200

    bottom nav bar fix

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,16 @@
   <div class="home-container">
     <AvatarCard title="Domme" class="shadow-md" />
     <AvatarCard title="Sukkel" class="shadow-md" />
+    <AvatarCard title="Domme" class="shadow-md" />
+    <AvatarCard title="Sukkel" class="shadow-md" />
+    <AvatarCard title="Domme" class="shadow-md" />
+    <AvatarCard title="Sukkel" class="shadow-md" />
+    <AvatarCard title="Domme" class="shadow-md" />
+    <AvatarCard title="Sukkel" class="shadow-md" />
+    <AvatarCard title="Domme" class="shadow-md" />
+    <AvatarCard title="Sukkel" class="shadow-md" />
+    <AvatarCard title="Domme" class="shadow-md" />
+    <AvatarCard title="Sukkel" class="shadow-md" />
   </div>
 </template>
 
