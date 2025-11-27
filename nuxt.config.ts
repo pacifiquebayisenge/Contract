cab342c4c68c0ac76efe65256d1184315e012a7c
Honore Pacifique Bayisenge <hpacifique@live.be>
2025-11-27 06:20:42 +0100

    skip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -52,7 +52,7 @@ export default defineNuxtConfig({
     },
   },
 
-  css: ['assets/main.scss', 'assets/css/tailwind.css', 'assets/css/pwa-standalone.css'],
+  css: ['assets/main.scss', 'assets/css/tailwind.css'],
 
   ssr: false,
   dirs: ['utils'],
