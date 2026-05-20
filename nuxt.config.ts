bd28df8459473cc321abe0612815346278a6e67f
Pacifique Stormz <hpacifique@live.be>
2026-05-20 18:02:03 +0200

    hash mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,6 +36,12 @@ export default defineNuxtConfig({
 		},
 	},
 
+	router: {
+		options: {
+			hashMode: true,
+		},
+	},
+
 	anime: {
 		composables: true, // Allows use of useAnime() composable
 		autoImport: true, // Auto-imports composables globally (if composables: true)
