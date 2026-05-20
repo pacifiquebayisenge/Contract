7f53243a1df6c92c25f99e1a956e509fa41c4506
Pacifique Stormz <hpacifique@live.be>
2026-05-20 19:08:15 +0200

    bottm nav bar correction

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,7 +21,7 @@
 				</div>
 
 				<div
-					class="fixed bottom-[4rem] left-0 right-0 z-50 flex justify-center items-center pointer-events-none !bg-transparent"
+					class="fixed bottom-[6rem] left-0 right-0 z-50 flex justify-center items-center pointer-events-none !bg-transparent"
 				>
 					<div class="pointer-events-auto">
 						<BottomMenuBar />
